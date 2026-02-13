67871485324590391c099f838a01e1c799e914b8
mei-v-opps <meihuaqu22@gmail.com>
2026-02-12 23:56:14 -0600

    Revert "added tests for invalid operations"
    This reverts commit 4f774b03616d8d0f8d7cd4c39cf2dbc74426380d.

--- tests/tests_1b.py
+++ tests/tests_1b.py
@@ -7,12 +7,6 @@ This module contains unit tests for the simple_calculator function defined in la
 import pytest
 from labs.lab_1.lab_1b import simple_calculator
 
-def test_invalid_operations():
-    with pytest.raises(ValueError, match="Invalid operation. Please choose from 'add', 'subtract', 'multiply', or 'divide'."):
-        simple_calculator("modulus", 5, 3)
-    with pytest.raises(ValueError, match="Invalid operation. Please choose from 'add', 'subtract', 'multiply', or 'divide'."):
-        simple_calculator("", 5, 3)
-
 def test_addition():
     assert simple_calculator("add", 5, 3) == 8          # Test for positive numbers
     assert simple_calculator("add", -2, 2) == 0         # Test for negative and positive number
