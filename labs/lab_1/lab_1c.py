0a700a2978f23b26e46d908babd84ddba51ee55e
mei-v-opps <meihuaqu22@gmail.com>
2026-03-04 08:52:16 -0600

    js marking what ive actually finishe

--- labs/lab_1/lab_1c.py
+++ labs/lab_1/lab_1c.py
@@ -8,7 +8,7 @@ Do not assume anything. Account for all edge cases.
 Derived from LeetCode problem: https://leetcode.com/problems/maximum-subarray/ (leetcode medium)
 """
 
-# TODO: Find and resolve the bug in the following implementation. Create unit tests to verify your fix.
+# TODO DONE: Find and resolve the bug in the following implementation. Create unit tests to verify your fix.
 def max_subarray_sum(nums: list[int]) -> int:
     """
     Function that takes in a list of integers and returns the maximum sum of any contiguous subarray.
--- labs/lab_1/lab_1d.py
+++ labs/lab_1/lab_1d.py
@@ -8,7 +8,7 @@ You may assume that each input would have exactly one solution, and you may not 
 Derived from LeetCode problem: https://leetcode.com/problems/two-sum/ (leetcode easy)
 """
 
-# TODO: Find and resolve the bug in the following implementation. Create unit tests to verify your fix.
+# TODO DONE: Find and resolve the bug in the following implementation. Create unit tests to verify your fix.
 def two_sum(nums: list[int], target: int) -> list[int]:
     """
     Function that takes in a list of integers and a target integer, and returns the indices of the two numbers that add up to the target.
